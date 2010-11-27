50dba6189e2635eafca1c94baa0e58ebaf2f6f69
Petrus Repo <petrus.repo@iki.fi>
2010-11-27 19:07:04 +0200

    separate app config from repositoyr

--- environment.rb
+++ environment.rb
@@ -1,26 +1,20 @@
 require 'rubygems'
 require 'dm-core'
-#require 'dm-timestamps'
-#require 'dm-validations'
-#require 'dm-aggregates'
-#require 'dm-migrations'
-#require 'haml'
-require 'ostruct'
+require 'app_config'
 
 require 'sinatra' unless defined?(Sinatra)
 
 configure do
-  SiteConfig = OpenStruct.new(
-                 :title => 'Apila',
-                 :author => 'Petrus Repo',
-                 :url_base => 'http://localhost:4567/'
-               )
 
   # load models
   $LOAD_PATH.unshift("#{File.dirname(__FILE__)}/lib")
   Dir.glob("#{File.dirname(__FILE__)}/lib/*.rb") { |lib| require File.basename(lib, '.*') }
 
-#  DataMapper.setup(:default, (ENV["DATABASE_URL"] || "sqlite3:///#{File.expand_path(File.dirname(__FILE__))}/#{Sinatra::Base.environment}.db"))
-  DataMapper.setup(:default, "oracle://USERNAME:PASSWORD@localhost/xe")
+  DataMapper.setup(:default, AppConfig.database_url)
 
+  # Test environment's config
+  configure :test do
+    DataMapper.setup(:default, "sqlite::memory:")
+  end
+  
 end
