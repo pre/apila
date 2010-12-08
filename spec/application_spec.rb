fa425226db146c5d79f13104502d1db49afc6426
Petrus Repo <petrus.repo@iki.fi>
2010-12-08 15:08:47 +0200

    fixed confusion with stubbing; remove twot test cases which are no longer needed

--- spec/application_spec.rb
+++ spec/application_spec.rb
@@ -7,50 +7,26 @@ describe 'Application' do
     Sinatra::Application.new
   end
 
-  def json_output(attribute, value)
-    "\"#{attribute}\":#{value}"
-  end
-
   specify 'should show the default index page' do
     get '/'
     last_response.should be_ok
   end
 
-  describe 'XML builder' do
-    specify 'should find object' do
-      ringer_id = 10001
-      ringer = Factory.build(:ringer, :id => ringer_id)
-      Ringer.should_receive(:first).with(:id => "#{ringer_id}").and_return(ringer)
-
-      get "/ringers/#{ringer_id}.xml"
-      last_response.status.should == 200
-    end
-
-    specify 'should return error code 404 when object is not found' do
-      ringer_id = 9999999
-      Ringer.should_receive(:first).with(:id => "#{ringer_id}").and_return(nil)
-
-      get "/ringers/#{ringer_id}.xml"
-      last_response.status.should == 404
-    end
-
-  end
-
   describe 'API XML responses' do
+
     specify 'should return ringer xml' do
       id = 10000
-      email = "testeri@yksityinen.osoite.example.com"
-      ringer = Factory.build(:ringer, :id => id, :email => email)
+      ringer = Factory.build(:ringer, :id => id)
       Ringer.stub!(:first).and_return(ringer)
 
       get "/ringers/#{id}.xml"
       last_response.should be_ok
-      last_response.body.should include("<email>#{email}</email>")
+      last_response.body.should include("<email>#{ringer.email}</email>")
       last_response.body.should include("<name>#{ringer.name}</name>")
     end
 
     describe 'for municipalities' do
-      specify 'should return municipality xml with environment centre' do
+      specify 'should include environment centre in municipality xml' do
         code = "AITOLA"
         municipality = Factory.build(:municipality, :code => code)
         Municipality.stub!(:first).and_return(municipality)
@@ -61,36 +37,33 @@ describe 'Application' do
         last_response.body.should include("<name>#{municipality.environment_centre.name}")
       end
 
-      specify 'should return an xml array of all municipalities' do
-        municipalities = []
-        10.times { |n| municipalities << Factory.create(:municipality, :code => "Municipality no #{n}") }
-        ## FIXME: Sholuld stub, but don't know how to return DataMapper::Collection objects who have #to_xml 
-        ## (in order to replace .create with .build above)
-        # Municipality.stub!(:all).and_return(municipalities)
+      specify 'should produce an xml set of all municipalities' do
+        municipalities = mock(DataMapper::Collection)
+        municipalities.should_receive(:to_xml)
+
+        Municipality.should_receive(:filter_by_code).and_return(municipalities)
 
         get "/municipalities.xml"
         last_response.should be_ok
-        municipalities.each do |municipality|
-          last_response.body.should include("<code>#{municipality.code}</code>")
-        end
       end
 
-      specify 'should return 404 for municipalities with unknown format' do
-        get "/municipalities.xyz"
-        last_response.status.should be(404)
+      specify 'should return code 200 when filtered resultset is empty' do
+        get "/municipalities.xml?code=DOES_NOT_EXIST"
+        last_response.status.should be(200)
       end
 
     end
 
     describe 'for species' do
-      specify 'should return an xml array of all species' do
-        code = "AOIUMQ"
-        species = Factory.create(:species, :code => code)
-        ## FIXME: look at. municipalities; Species.stub!(:filter_by_code).and_return(species)
+
+      specify 'should produce an xml set of all species' do
+        species = mock(DataMapper::Collection)
+        species.should_receive(:to_xml)
+
+        Species.should_receive(:filter_by_code).and_return(species)
 
         get "/species.xml"
         last_response.should be_ok
-        last_response.body.should include("<code>#{code}</code>")
       end
 
       specify 'should include species name in the response' do
@@ -104,7 +77,7 @@ describe 'Application' do
         last_response.body.should include("Haikara", "Stork")
       end
 
-      specify 'should return code 200 when nothing is found' do
+      specify 'should return code 200 when filtered resultset is empty' do
         get "/species.xml?code=DOES_NOT_EXIST"
         last_response.status.should be(200)
       end
