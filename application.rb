86f4e8087cb171491905592a475ce8c04671a017
Petrus Repo <petrus.repo@iki.fi>
2010-12-08 17:31:20 +0200

    keep it dry

--- application.rb
+++ application.rb
@@ -29,41 +29,30 @@ get '/' do
 end
 
 get '/ringers/:id.xml' do
-  ringer = Ringer.first(:id => params[:id])
-
-  if ringer
-    ringer.to_xml(:methods => [:name], :only => Ringer.shared_attributes)
-  else
-    halt 404
-  end
+  respond_with(Ringer.first(:id => params[:id]), { :methods => [:name], 
+                                                   :only => Ringer.shared_attributes })
 end
 
 get '/municipalities/:code.xml' do
-  municipality = Municipality.find_by_code(params[:code])
-
-  if municipality
-    municipality.to_xml(:methods => [:environment_centre], :only => Municipality.shared_attributes)
-  else
-    halt 404
-  end
+  respond_with(Municipality.find_by_code(params[:code]), { :methods => [:environment_centre], 
+                                                           :only => Municipality.shared_attributes })
 end
 
 get '/municipalities.xml' do
   municipalities = Municipality.filter_by_code(params[:code])
-
-  if municipalities
-    municipalities.to_xml(:methods => [:environment_centre], :only => Municipality.shared_attributes)
-  else
-    halt 404
-  end
+  municipalities.to_xml(:methods => [:environment_centre], :only => Municipality.shared_attributes)
 end
 
 get '/species.xml' do
   species = Species.filter_by_code(params[:code])
-
-  if species
-    species.to_xml(:methods => [:names])
-  else
-    halt 404
-  end
+  species.to_xml(:methods => [:names])
 end
+
+private
+  def respond_with(object, opts = {})
+    if object
+      object.to_xml(opts)
+    else
+      halt 404
+    end
+  end
